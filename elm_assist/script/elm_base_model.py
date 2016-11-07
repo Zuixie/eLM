d8b7f57dd1c402a5dee8b1db4ffc491ac64a749b
David_Wang <791706848@qq.com>
2016-11-07 23:58:52 +0800

    init elm_base_model

---
+++ elm_assist/script/elm_base_model.py
@@ -0,0 +1,59 @@
+# /usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import openpyxl
+
+# 模型1 获取数据库中的数据 再对excel中的每一行进行处理
+class BaseOneByOne:
+    def __init__(self, filename, sheet = 0, startline = 0, server = None):
+        print ("init", filename, sheet, startline, server)
+        self.db = None
+        self.filename = filename
+        self.workbook = openpyxl.load_workbook(filename = filename)
+        self.sheet = self.workbook.get_sheet_by_name(self.workbook.get_sheet_names()[sheet])
+        self.startline = startline
+
+    def before_run_start(self):
+        pass
+
+    def readline(self, line_no, row):
+        print (line_no, row[0].value)
+    
+    def create_sql(self):
+        pass
+
+    def run_query(self):
+        pass
+
+    def handle_line(self, line_no, row):
+        print (line_no, row[0].value)
+
+    def after_run_end(self):
+        pass
+
+    def run(self):
+        self.before_run_start()
+
+        rows = tuple(self.sheet.rows)
+        for index, item in enumerate(rows[self.startline:], self.startline):
+            self.readline(index, item)
+        
+        self.run_query()
+        # sql select
+        print ("start handle ===================================")
+        for index, item in enumerate(rows[self.startline:], self.startline):
+            self.handle_line(index, item)
+
+        self.after_run_end()
+
+    def __del__(self):
+        print("de init")
+        if self.db:
+            self.db.close()
+
+def run():
+    base1 = BaseOneByOne(filename = "/home/david/sample.xlsx", startline = 1)
+    base1.run()
+
+if __name__ == "__main__":
+    run()
